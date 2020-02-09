e651ccd9fd26fd9e4ce6fad3796e26ffcf0eecd1
Praty21 <44113506+Praty21@users.noreply.github.com>
2020-02-10 00:01:18 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,4 +18,4 @@ fi
 # Redirect output from this script to an "edge.properties" file in Jenkins. 
 echo EdgeConfigOptions=$EdgeConfigOptions
 
-mvn -f HR-API/pom.xml install -Pprod -Dusername=${apigeeUsername} -Dpassword=${apigeePassword} -Dapigee.config.options=$EdgeConfigOptions
+mvn -f C:/apigee-cicd-master/hr-api/pom.xml install -Pprod -Dusername=pratysin@gmail.com -Dpassword=Qwaszx@123 -Dapigee.config.options=$EdgeConfigOptions
